5fb07da185484e750bf133399490329fe32f80ae
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-01-15 19:15:24 +0300

    Initial Commit

---
+++ FotografPaylasmaUygulamasiTests/FotografPaylasmaUygulamasiTests.swift
@@ -0,0 +1,17 @@
+//
+//  FotografPaylasmaUygulamasiTests.swift
+//  FotografPaylasmaUygulamasiTests
+//
+//  Created by Ahmet Hakan Asaroğlu on 15.01.2025.
+//
+
+import Testing
+@testable import FotografPaylasmaUygulamasi
+
+struct FotografPaylasmaUygulamasiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
